01b260d63712e2136468391b0a404f613a375ba6
Nitin Sakhare <nitinsakhare6@gmail.com>
2019-10-16 13:11:49 +0530

    Initial Commit

---
+++ Movie Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Browser
+//
+//  Created by Nitin on 16/10/19.
+//  Copyright © 2019 Nitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
